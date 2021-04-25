7260fb6eb7fdea7225774d9033df72620fad22ac
oleng <oleng@users.noreply.github.com>
2021-04-25 11:33:57 +0700

    removed bitwarden, decided app store is more secure. replaced sublime & pycharm with vs code as default

--- brew_install_packages.sh
+++ brew_install_packages.sh
@@ -1,4 +1,4 @@
 #!/usr/local/env bash
 
 brew install ntfs-3g smartmontools tree bitwarden-cli python docker youtube-dl
-brew cask install bitwarden miniconda sublime-text pycharm-ce
+brew cask install visual-studio-code
